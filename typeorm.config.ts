d2c324f8bb3da6b01c5044112820658f5be4cdd2
HMQui <huynhqui6425@gmail.com>
2026-06-09 13:41:39 +0700

    config db for pro

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -5,13 +5,18 @@ config();
 
 const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
+    // For production
+    url: process.env.DATABASE_URL,
+
+    // For dev
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 5432,
     username: process.env.DB_USER || 'admin',
     password: process.env.DB_PASSWORD || 'secretpassword',
     database: process.env.DB_NAME || 'g_scores',
-    entities: ['src/**/*.entity.ts'],
-    migrations: ['src/migrations/*.ts'],
+
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['dist/migrations/*.js'],
     synchronize: false,
 };
 
